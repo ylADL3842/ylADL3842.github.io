40d4eb54559c9c68f75b998bde434af6b14c7ec6
yida.lin@hotmail.com <ylADL3842>
2019-04-14 01:27:13 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -10,11 +10,11 @@ const examples = {
   'example1':
       'Alice was beginning to get very tired of sitting by her sister on the bank.',
   'example2':
-      'Buda-Pesth seems a wonderful place.',
+      "-- ECKERMANN ' S CONVERSATIONS WITH GOETHE .",
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.',
+      'Him to unthrone we then May hope , when everlasting Fate shall yield To fickle Chance , and Chaos judge the strife .',
   'example4':
-      'I rose to go, but Holmes caught me by the wrist and pushed me back into my chair.'   
+      "Fye , ' tis a fault to Heauen , A fault against the Dead , a fault to Nature , To Reason most absurd , whose common Theame Is death of Fathers , and who still hath cried , From the first Coarse , till he that dyed to day , This must be so ."   
 };
 
 function status(statusText) {
